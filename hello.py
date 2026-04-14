1f056194118a96b81a5047a63bf33a1572713b0f
boroborobo <hw04184@gmail.com>
2026-04-14 10:24:35 +0900

    브랜치에서 테스트 코드 추가
    Add greeting message for test-branch

--- hello.py
+++ hello.py
@@ -1,7 +1,11 @@
 # 나의 첫 번째 GitHub 파일이다뉴
 print("Hello, GitHub!")
+
 # 변수에 내 이름을 저장한다뉴
 name = "boroborobo"
 
 # 인사를 출력한다뉴
 print(f"{name}의 GitHub 연습장에 온 걸 환영한다뉴!")
+
+# 브랜치에서 실험하는 코드다뉴
+print("이건 test-branch에서 추가한 코드다뉴!")
